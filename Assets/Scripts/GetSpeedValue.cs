8ebfb4dc3727c69b6ffdf0a310a3dd57b0345124
Alexandre Costa <alexandre97costa@gmail.com>
2023-05-22 18:15:31 +0100

    Update GetSpeedValue.cs

--- Assets/Scripts/GetSpeedValue.cs
+++ Assets/Scripts/GetSpeedValue.cs
@@ -8,18 +8,22 @@ public class GetSpeedValue : MonoBehaviour
     public TMP_Text text;
     public GameObject Player;
     public InertiaMovement PlayerMovement;
+    public CharacterController PlayerController;
     public float PlayerCurrentSpeed;
+    public float PlayerVelocity;
     // Start is called before the first frame update
     void Start()
     {
         text = gameObject.GetComponent<TMP_Text>();
         PlayerMovement = Player.GetComponent<InertiaMovement>();
+        PlayerController = Player.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         PlayerCurrentSpeed = Mathf.Round(PlayerMovement.currentTotalSpeed * 100.0f) / 100.0f;
-        text.text = "Speed: " + PlayerCurrentSpeed;
+        PlayerVelocity = Mathf.Round(PlayerController.velocity.magnitude * 100.0f) / 100.0f;
+        text.text = "Speed Value: " + PlayerCurrentSpeed + "\nActual Velocity: " + PlayerVelocity;
     }
 }
