ac6c9966497fbd52ccd617268fa8c353a7687628
Joao Sequeira <joaosequeira28@hotmail.com>
2023-05-29 17:29:56 +0100

    começo da integração do som

--- Assets/Scripts/Monobehaviours/AnimationsControllers.cs
+++ Assets/Scripts/Monobehaviours/AnimationsControllers.cs
@@ -4,44 +4,52 @@ using UnityEngine;
 
 public class AnimationsControllers : MonoBehaviour
 {
+    [Header("Animator")]
     [SerializeField] private AnimatorOverrideController[] overrideControllers;
     [SerializeField] private AnimationOverrider overrider;
     private Animator animator;
 
+    [Space(10)]
+    [Header("Sounds")]
+    public AudioSource footSteps;
+    private float maxPlayerSpeed = 10f;
+    private float maxPitchMultiplier = 2f;
+
     private void Awake()
     {
+        footSteps = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
     }
 
     public void Set(int value) { overrider.SetAnimations(overrideControllers[value]); }
 
-    public void IdleAction() { Set(15); animator.SetBool("isAction", false); }
+    public void IdleAction() { Set(15); animator.SetBool("isAction", false); DisableStepsSound(); }
 
     public void FallAction() { Set(0); animator.SetBool("isAction", true); }
 
     public void JumpAction() { Set(1); animator.SetBool("isAction", true); }
 
-    public void RunBackAction() { Set(2); animator.SetBool("isAction", true); }
+    public void RunBackAction() { Set(2); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void RunLeftBackAction() { Set(3); animator.SetBool("isAction", true); }
+    public void RunLeftBackAction() { Set(3); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void RunRightBackAction() { Set(4); animator.SetBool("isAction", true); }
+    public void RunRightBackAction() { Set(4); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void RunAction() { Set(5); animator.SetBool("isAction", true); }
+    public void RunAction() { Set(5); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void RunLeftAction() { Set(6); animator.SetBool("isAction", true); }
+    public void RunLeftAction() { Set(6); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void RunRightAction() { Set(7); animator.SetBool("isAction", true); }
+    public void RunRightAction() { Set(7); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void SprintAction() { Set(8); animator.SetBool("isAction", true); }
+    public void SprintAction() { Set(8); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void SprintLeftAction() { Set(9); animator.SetBool("isAction", true); }
+    public void SprintLeftAction() { Set(9); animator.SetBool("isAction", true); EnableStepsSound(); }
     
-    public void SprintRightAction() { Set(10); animator.SetBool("isAction", true); }
+    public void SprintRightAction() { Set(10); animator.SetBool("isAction", true); EnableStepsSound(); }
 
-    public void StrafeLeftAction() { Set(11); animator.SetBool("isAction", true); }
+    public void StrafeLeftAction() { Set(11); animator.SetBool("isAction", true); EnableStepsSound(); }
     
-    public void StrafeRightAction() { Set(12); animator.SetBool("isAction", true); }
+    public void StrafeRightAction() { Set(12); animator.SetBool("isAction", true); EnableStepsSound(); }
     
     public void RunJumpAction() { Set(13); animator.SetBool("isAction", true); }
 
@@ -59,4 +67,15 @@ public class AnimationsControllers : MonoBehaviour
     
     public void LandAction() { Set(21); animator.SetBool("isAction", true); }
 
+    private void EnableStepsSound()
+    {
+        footSteps.enabled = true;
+        footSteps.Play();
+    }
+
+    private void DisableStepsSound()
+    {
+        footSteps.enabled = false;
+        footSteps.Stop();
+    }
 }
