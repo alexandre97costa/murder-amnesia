9e41fbde9b88e00a9b79376264be28617645e25b
Joao Sequeira <95438126+JoaoSeque1ra@users.noreply.github.com>
2023-07-05 14:50:33 +0100

    cutscene2

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -9,7 +9,7 @@ public class ChangeScene : MonoBehaviour
     [Header("Choose Scene")]
     public ListScene sceneSelect;
     private List<string> listScene = new List<string> {
-        "Cha1_Lvl2", "Cha1_Lvl3", "Cha2_Lvl1", "Cha2_Lvl2", "Cha2_Lvl3", "Cha3_Lvl1", "End", "DemoTutorial", "TesteCutScene"
+        "Cha1_Lvl2", "Cha1_Lvl3", "Cutscene2", "Cha2_Lvl1", "Cha2_Lvl2", "Cha2_Lvl3", "Cha3_Lvl1", "End", "DemoTutorial", "TesteCutScene"
     };
 
     [Space(10)]
@@ -20,13 +20,14 @@ public class ChangeScene : MonoBehaviour
     {
         Cha1_Lvl2 = 0,
         Cha1_Lvl3 = 1,
-        Cha2_Lvl1 = 2,
-        Cha2_Lvl2 = 3,
-        Cha2_Lvl3 = 4,
-        Cha3_Lvl1 = 5,
-        End = 6,
-        DemoTutorial = 7,
-        TesteCutScene = 8,
+        Cutscene2 = 2,
+        Cha2_Lvl1 = 3,
+        Cha2_Lvl2 = 4,
+        Cha2_Lvl3 = 5,
+        Cha3_Lvl1 = 6,
+        End = 7,
+        DemoTutorial = 8,
+        TesteCutScene = 9,
     }
 
     private void OnCollisionEnter(Collision collision)
