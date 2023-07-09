5ddcd0f0a1fe18b9ac7cbfebda7d20d8a55685f7
Hugo <goncalvesc.hugo@gmail.com>
2023-07-09 17:13:37 +0100

    script cartas

--- Assets/Scripts/Cartas.cs
+++ Assets/Scripts/Cartas.cs
@@ -1,47 +1,51 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-// public class Cartas : MonoBehaviour
-// {
-//     [SerializeField]
-//     private Image _noteImage;
+public class Cartas : MonoBehaviour
+{
+    [SerializeField]
+    public Image _noteImage;
 
-//     public GameObject MessagePanel;
-//     public bool Action = false;
+    public GameObject MessagePanel;
+    public bool Action = false;
 
-//     public void Start()
-//     {
-//         MessagePanel.SetActive(false);
-//     }
+    public void Start()
+    {
+        MessagePanel.SetActive(false);
+        _noteImage.enabled = false;
+    }
 
-//     public void Update()
-//     {
-//         if(Input.GetKeyDown(KeyCode.E))
-//         {
-//             MessagePanel.SetActive(false);
-//             Action = false;
-//             _noteImage.enable = true;
-//         }
-//     }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(Action == true)
+            {
+                MessagePanel.SetActive(false);
+                Action = false;
+                _noteImage.enabled = true;
+            }
+        }
+    }
 
-//     void OnTriggerEnter(Collider other)
-//     {
-//         if(other.CompareTag("Player"))
-//         {
-//             MessagePanel.SetActive(true);
-//             Action = true;
-//         }
-//     }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            MessagePanel.SetActive(true);
+            Action = true;
+        }
+    }
 
-//     void OnTriggerExit(Collider other)
-//     {
-//         if(other.CompareTag("Player"))
-//         {
-//             MessagePanel.SetActive(false);
-//             Action = false;
-//             _noteImage.enable = false;
-//         }
-//     }
-
-// }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            MessagePanel.SetActive(false);
+            Action = false;
+            _noteImage.enabled = false;
+        }
+    }
+}
